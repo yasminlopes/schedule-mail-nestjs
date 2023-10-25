97d4576dd3c810019c46557e9689b13f5fea317b
Yasmin Lopes <yclopes27@gmail.com>
2023-10-25 13:52:13 -0300

    chore: clean up codebase by removing unused code

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { SendgridModule } from './app/sendgrid/sendgrid.module';
 
 @Module({
   imports: [SendgridModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
