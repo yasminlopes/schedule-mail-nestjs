e986a92f4144f71a2902f0194b47a85a6bb51ddf
Yasmin Lopes <yclopes27@gmail.com>
2023-10-26 13:56:00 -0300

    feat(mail): import SendgridModule for email sending in MailModule

--- src/app/mail/mail.module.ts
+++ src/app/mail/mail.module.ts
@@ -3,10 +3,12 @@ import { MailService } from './services/mail/mail.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { MailEntity } from './entity/mail.entity';
 import { MailController } from './controller/mail/mail.controller';
+import { MailCron } from './cron/mail.cron';
+import { SendgridModule } from '../sendgrid/sendgrid.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([MailEntity])],
-  providers: [MailService],
+  imports: [TypeOrmModule.forFeature([MailEntity]), SendgridModule],
+  providers: [MailService, MailCron],
   controllers: [MailController],
 })
 export class MailModule {}
