e95fbd5cab2dabee326021e2bd848e496446886e
Yasmin Lopes <yclopes27@gmail.com>
2023-10-26 13:58:51 -0300

    feat(mail): add findAll method to MailService for filtering emails by status or due date

--- src/app/mail/services/mail/mail.service.ts
+++ src/app/mail/services/mail/mail.service.ts
@@ -14,4 +14,19 @@ export class MailService {
   public async save(data: SaveMailDto): Promise<MailEntity> {
     return this.mailRepository.save(this.mailRepository.create(data));
   }
+
+  public async findAll(params?: Partial<FindAllMailDto>) {
+    const query = this.mailRepository.createQueryBuilder('mail');
+
+    if (params?.dueDateLte) {
+      query.andWhere('mail.dueDate <= :dueDateLte', { dueDateLte: params.dueDateLte });
+    }
+
+    if (params?.status) {
+      query.andWhere('mail.status = :status', { status: params.status });
+    }
+
+    return query.getMany();
+  }
+
 }
